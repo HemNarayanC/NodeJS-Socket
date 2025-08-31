08cacf416f4d287339e0cedec101053df63a0930
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-08-31 08:16:48 +0545

    client side setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <input placeholder='Enter your message...'/>
+      <button>Send</button>
+    </div>
+  )
+}
+
+export default App
