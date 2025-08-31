6ae38d2b2dfb17c64f503af495c5b02a182bfd96
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-08-31 09:31:57 +0545

    send message functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { io } from 'socket.io-client'
+const socket = io.connect(import.meta.env.VITE_SERVER_URL);
 
 const App = () => {
+  const [sndMessage, setSndMessage] = useState("");
+  const sendMessage = () => {
+    socket.emit("send_message", {message: sndMessage});
+  }
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      alert(data.message);
+    })
+  }, [socket])
+
   return (
     <div>
-      <input placeholder='Enter your message...'/>
-      <button>Send</button>
+      <input placeholder='Enter your message...' onChange={(e) => {
+        setSndMessage(e.target.value)
+      }}/>
+      <button onClick={sendMessage}>Send</button>
+      <div>
+        <h1>Received Message</h1>
+      </div>
     </div>
   )
 }
