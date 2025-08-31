8dc156d7637c34cc51a47ec914f492d895661071
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-08-31 07:45:12 +0545

    set up basic express server with http and Socket.IO

---
+++ server/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const http = require('http');
+const express = require('express')
+const { Server } = require('socket.io');
+const app = express();
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+});
